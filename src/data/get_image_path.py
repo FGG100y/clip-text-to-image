8744dd013e50ca40b6642d67b34be2fc2cefcad0
fgg100y <brantvan49@outlook.com>
2024-07-31 21:55:16 +0800

    Fix typo

--- src/data/get_image_path.py
+++ src/data/get_image_path.py
@@ -2,14 +2,14 @@ import itertools
 from pathlib import Path
 
 
-def read(img_dir="data/raw/weixin_img/", patterns=("*.jpg", "*.png"), no_posix=False):
-    """Get image's filepath"""
+def read(img_dir="data/raw/weixin_img/", patterns=("*.jpg", "*.png"), as_posix=False):
+    """Get image's filepath [default from data/raw/weixin_img/]"""
     data_path = Path(img_dir)
     image_path = list(
         itertools.chain.from_iterable(data_path.glob(pattern) for pattern in patterns)
     )
 
-    if no_posix:
+    if as_posix:
         return [imgp.as_posix() for imgp in image_path]
 
     return image_path
